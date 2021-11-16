56cfcac18f9043d16eefdf12f0f7457fa0b43494
HARSH HG GARG <harshgargthapar@gmail.com>
2021-11-17 01:04:43 +0530

    added slider and offers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import PreNavbar from './components/PreNavbar';
-import Navbar from './components/Navbar.js'
-
+import Navbar from './components/Navbar.js';
+import  { BrowserRouter as Router} from 'react-router-dom';
+import Slider from "./components/Slider.js" ;
+import  data from "./data/data.json" ;
+import Offers from "./components/Offers.js"
 function App() {
   return (
-    <div>
+    <Router>
       <PreNavbar /> 
       <Navbar />
-      
-    </div>
+      <Slider start={data.banner.start} /> 
+      <Offers offer={data.offer} />
+    </Router>
+
   )
-}
+};
 
 export default App;
