9b2000a59c7a474c7b0f469510bdc2bb5a9eb6ee
HARSH HG GARG <harshgargthapar@gmail.com>
2021-11-16 15:37:05 +0530

    added NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import PreNavbar from './components/PreNavbar';
+import Navbar from './components/Navbar.js'
 
 function App() {
   return (
     <div>
       <PreNavbar /> 
+      <Navbar />
       
     </div>
   )
