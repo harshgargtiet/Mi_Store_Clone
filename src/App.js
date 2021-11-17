5723dfd42d5460771423c9d8841a2d4bf16fe5c2
HARSH HG GARG <harshgargthapar@gmail.com>
2021-11-17 18:24:39 +0530

    Done with clonning

--- src/App.js
+++ src/App.js
@@ -12,12 +12,17 @@ import HotAccessoriesMenu from "./components/HotAccessoriesMenu.js"
 import HotAccessories from "./components/HotAccessories.js";
 import ProductReviews from "./components/ProductReviews.js"
 import Videos from "./components/Videos.js";
+import  Banner from "./components/Banner.js";
+import Footer from "./components/Footer.js" ;
+import NavOptios from "./components/NavOptios.js";
 
 function App() {
   return (
     <Router>
       <PreNavbar /> 
       <Navbar />
+      <NavOptios miPhones={data.miPhones} redmiPhones={data.redmiPhones} tv={data.tv} laptop={data.laptop} fitnessAndLifeStyle={data.fitnessAndLifeStyle} home={data.home} audio={data.audio}  accessories={data.accessories}/>
+   
       <Slider start={data.banner.start} /> 
       <Offers offer={data.offer} />
       <Heading text ="STAR PRODUCTS"/> 
@@ -45,6 +50,9 @@ function App() {
        <ProductReviews productReviews={data.productReviews} />
        <Heading text="VIDEOS"/>
        <Videos videos={data.videos} />
+       <Heading text="IN THE PRESS"/>
+       <Banner banner={data.banner}/>
+       <Footer footer={data.footer} />
     </Router>
 
   )
